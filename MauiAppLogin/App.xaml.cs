840101cfd1802d92cfd66368cb51c619379e4e94
Jopilot-git <jonathanholly@bol.com.br>
2025-05-03 17:12:38 -0300

    Arquivos finalizados

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -6,7 +6,27 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            string? usuario_logado = null;
+
+            Task.Run(async () =>
+
+            {
+                usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+
+                if (usuario_logado == null) 
+                {
+                    MainPage = new login();
+                
+                } else
+                
+                {
+                    MainPage = new protegida();
+                
+                }
+               
+            });
+
+            MainPage = new login();
         }
         protected override Window CreateWindow(IActivationState activationState)
         {
--- MauiAppLogin/login.xaml.cs
+++ MauiAppLogin/login.xaml.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+
 namespace MauiAppLogin;
 
 public partial class login : ContentPage
@@ -6,4 +8,55 @@ public partial class login : ContentPage
 	{
 		InitializeComponent();
 	}
-}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			List<DadosUsuario> lista_usuario = new List<DadosUsuario>()
+			{
+				new DadosUsuario()
+				{
+					Usuario = "Jose",
+					Senha = "123"
+				},				
+				new DadosUsuario() 
+				{
+					Usuario = "Maria",
+					Senha = "321"
+					
+				}				
+
+
+			};
+			DadosUsuario dados_digitados = new DadosUsuario()
+			{
+				Usuario = txt_usuario.Text,
+				Senha = txt_senha.Text
+
+			};
+
+			// LINQ
+			if (lista_usuario.Any(i => (dados_digitados.Usuario == i.Usuario && dados_digitados.Senha == i.Senha))) 
+			{
+				await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+				App.Current.MainPage = new protegida();
+			
+			} else 
+			
+			{
+				throw new Exception("Usuário ou senha incorreto.");
+			
+			
+			}
+
+
+
+		} catch(Exception ex) 
+		
+		{
+			await DisplayAlert("Ops", ex.Message, "Fechar");
+
+		}// Fecha o catch
+    } // fecha método
+}//fecha a classe
